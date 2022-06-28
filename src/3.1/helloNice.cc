b952743fcc4767810fa97c069ef774ae804b4cd8
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2022-06-28 16:19:10 +0200

    add task 3.1

---
+++ src/3.1/helloNice.cc
@@ -0,0 +1,13 @@
+#include <iostream> // std::cout std::endl
+
+/*
+ * program that prints line "Hello World!\n"
+ * to standard output
+ *
+ * returns: 0 on success
+ */
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0; // success
+}
---
+++ src/3.2/surprise.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+  int i{0};
+  std::cin >> i;
+  if (i = 1) {
+    std::cout << "input was 1" << std::endl;
+  }
+  return 0;
+}
