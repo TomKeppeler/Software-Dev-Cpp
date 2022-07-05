a4d3720b87808647c443d4dedf9034fbdeee6ba1
Tom Keppeler <tomkeppeler25@gmail.com>
2022-07-05 15:17:55 +0200

    add: 4.1

---
+++ src/4.1/MyAnd.cc
@@ -0,0 +1,23 @@
+bool myand(bool a, bool b){
+    if (a) {
+        if(b){
+            return true;
+        }else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+bool myAndAnd(bool a, bool b){
+    if (a) {
+        if (b) {
+            return true;
+        }else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
