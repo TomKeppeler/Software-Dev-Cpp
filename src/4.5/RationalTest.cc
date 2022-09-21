fb7bdf09dc8c60e88ef31b30bc1e92a09be69523
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2022-09-21 19:34:57 +0200

    Update RationalTest.cc

--- src/4.5/RationalTest.cc
+++ src/4.5/RationalTest.cc
@@ -1,7 +1,16 @@
 #include "Rational.hh"
-using namespace linalg;
 
-int main(){
-    Rational r{1,2};
-    r.print();
+int static const SIZE = 10;
+
+int main() {
+    Rational *r = new Rational[SIZE];
+    std::string ret = "";
+    for (int i = 0; i < SIZE; i++) {
+        r[i] = Rational{2*i+1, 2*i+5};
+        ret += r[i].numer() + "/" + r[i].denom();
+    }
+
+    std::cout << ret << std::endl;
+    delete r;
+    return 0; 
 }
