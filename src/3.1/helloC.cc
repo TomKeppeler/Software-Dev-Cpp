6a623f4ed05d91ffcbd741919705bf326e5e297f
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2022-09-22 14:02:46 +0200

    Update helloC.cc

--- src/3.1/helloC.cc
+++ src/3.1/helloC.cc
@@ -1,7 +1,15 @@
-#include <cstdio>
-
+#include "iostream"
+#include <ostream>
 int main()
 {
-  std::printf("Hello World!\n");
+  int* ptr;
+  int var = 7;
+  int foo = 21;
+  ptr = &var;
+  ptr = &foo;
+  int& ref = var;
+  ref = 19;
+  std::cout << "ref: " << ref << std::endl << "var: " << var <<std::endl
+  << "ptr: " << ptr << std::endl;
   return 0;
 }
